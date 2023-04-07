19c025fa26432d2094293bd84234c7e0a591cfa5
mounceryan <124623846+mounceryan@users.noreply.github.com>
2023-04-07 21:37:22 +0100

    Started cleaning up the css.

--- movies.js
+++ movies.js
@@ -37,7 +37,6 @@ function search1() {
             document.getElementById("search-results").style.display = "none";  
             document.getElementById("content").style.display = "block";
             document.getElementById("content").style.overflow = "initial";
-            document.getElementById("banner-link").style.display = "block";  
             document.getElementById("block").style.display = "none";  
             document.getElementById("button-box").style.display = "block";  
         } else {
@@ -48,7 +47,6 @@ function search1() {
             document.getElementById("search-results").style.display = "block";
             document.getElementById("content").style.display = "flex";
             document.getElementById("content").style.overflow = "auto";
-            document.getElementById("banner-link").style.display = "none";  
             document.getElementById("block").style.display = "block"; 
             document.getElementById("button-box").style.display = "none";  
         }
@@ -76,7 +74,6 @@ function sortList() {
       b[i].parentNode.parentNode.parentNode.parentNode.parentNode.insertBefore(b[i + 1].parentNode.parentNode.parentNode.parentNode, b[i].parentNode.parentNode.parentNode.parentNode);
       switching = true;
         
-        document.getElementById("banner-link").style.display = "none";  
             document.getElementById("block").style.display = "block"; 
         b[i].parentElement.parentElement.parentElement.parentElement.parentElement.style.whiteSpace = "normal";
         b[i].parentElement.parentElement.parentElement.parentElement.parentElement.style.textAlign = "center";
@@ -95,53 +92,6 @@ function sortList() {
        
     }
   }
-}
-
-//Banner
-
-window.addEventListener("load", bannerimages, false);
-
-function bannerimages(){
-    
-var links = [
-  "https://www.youtube.com/embed/rwBkQnps-x8",
-  "https://www.youtube.com/embed/l37CMUicZFM",
-  "https://1drv.ms/v/s!AguK73hSjArpjapY41tPJSIw1iPypg?e=dMWLCm"
-]
-    
-var images = [
-  "banners/once.jpeg",
-  "banners/pulp.jpg",
-  "banners/odyssey.jpg"
-]
-
-var titles = [
-  "<span style='color:#f1af1d; font-size:3vw;'>Once Upon A Time in... </span><br>Hollywood",
-  "<span style='color:#f2da01; font-size:6.3vw; letter-spacing: -4px; text-shadow: -1px 1px #000000; transform:scale(0.6,1); display: inline-block;'>PULP FICTION</span>",
-  "2OO1: A SPACE ODYSSEY"
-]
-
-var fonts = [
-  "hollywood-hills",
-  "aachen-bold",
-  "gill-sans-light"
-]
-
-var link = document.getElementById("banner-link");
-var banner = document.getElementById("banner");
-var title = document.getElementById("banner-title");
-var i = 0;
-
-setInterval(function() {
-      link.href = links[i];
-      title.innerHTML = titles[i];
-      title.style.fontFamily = fonts[i];
-      banner.style.backgroundImage = "url(" + images[i] + ")";
-      i = i + 1;
-      if (i == images.length) {
-      	i =  0;
-      }
-}, 5000);
 }
 
 //Refresh Page
